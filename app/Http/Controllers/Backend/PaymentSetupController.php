9357a2e980450f244e4e5c44843d6983e425bdde
Anmol <anmol@curvesncolors.ch>
2023-02-01 10:10:22 +0545

    PaymentSetupService created

--- app/Http/Controllers/Backend/PaymentSetupController.php
+++ app/Http/Controllers/Backend/PaymentSetupController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-use App\Models\PaymentSetup;
-use App\Models\Client;
 use App\User;
-
+use App\Models\Client;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\PaymentSetupStore;
 use App\Http\Requests\PaymentSetupUpdate;
 use Illuminate\Support\Facades\Validator;
+use App\Services\Backend\PaymentSetupService;
 
 class PaymentSetupController extends Controller
 {
@@ -21,7 +19,7 @@ class PaymentSetupController extends Controller
             $client = Client::where('uuid', filter_var($request->client, FILTER_SANITIZE_STRING))->first();
             $data = $client->payment_setups ?? null;
         } else {
-            $data = PaymentSetup::orderBy('reference_date', 'ASC')->get();
+            $data = PaymentSetupService::_get();
         }
 
         $clients = Client::select('uuid', 'name')->get();
@@ -36,7 +34,7 @@ class PaymentSetupController extends Controller
 
     public function store(PaymentSetupStore $request)
     {
-       if (PaymentSetup::_storing($request)) {
+       if (PaymentSetupService::_storing($request)) {
             return redirect()->route('payment.setup.index')->with('success', 'The payment setup has been created.');
         }
         return back()->withInput()->with('error', 'Sorry, could not create payment setup at this time. Please try again later.');
@@ -44,7 +42,7 @@ class PaymentSetupController extends Controller
 
     public function edit($uuid)
     {
-        if ($data = PaymentSetup::where('uuid', $uuid)->first()) {
+        if ($data = PaymentSetupService::_find($uuid)) {
             $clients = Client::select('id', 'name', 'email')->where('is_active', 10)->get();
             return view('backend.payment.setup.edit', compact('data', 'clients'));
         }
@@ -53,7 +51,7 @@ class PaymentSetupController extends Controller
 
     public function update(PaymentSetupUpdate $request, $uuid)
     {
-        $update = PaymentSetup::_updating($request, $uuid);
+        $update = PaymentSetupService::_updating($request, $uuid);
         if ($update) {
             return redirect()->route('payment.setup.index')->with('success', 'The payment setup has been updated.');
         }
@@ -62,7 +60,7 @@ class PaymentSetupController extends Controller
 
     public function change_status($uuid)
     {
-        if (PaymentSetup::_change_status($uuid)) {
+        if (PaymentSetupService::_change_status($uuid)) {
             return back()->with('success', 'The payment setup status has been changed.');
         }
         return back()->with('error', 'Sorry, could not change status of the payment setup at this time. Please try again later.');
@@ -70,7 +68,7 @@ class PaymentSetupController extends Controller
 
     public function entry($uuid)
     {
-        if ($data = PaymentSetup::_entries($uuid)) {
+        if ($data = PaymentSetupService::_entries($uuid)) {
             return response()->json(['status' => true, 'entries' => $data['entries'], 'new_entry' => $data['new_entry']]);
         }
         return response()->json(['status' => false]);
@@ -80,7 +78,7 @@ class PaymentSetupController extends Controller
     {
         if ($request->has('master_password') && User::_check_master($request->master_password)) {
             if ($request->has('entries')) {
-                if (PaymentSetup::_sending($request->entries, $uuid)) {
+                if (PaymentSetupService::_sending($request->entries, $uuid)) {
                     return response()->json(['status' => true, 'link' => '', 'msg' => 'The payment link email(s) has been sent.']);
                 }
                 return response()->json(['status' => false, 'msg' => 'The payment setup you want to send does not exist.']);
